4e8d924a21154b1fdf8283259ed585ab8635947f
alpacaShiro <shirolong76@gmail.com>
2022-03-05 21:59:13 +0800

    input

---
+++ test/aly.java
@@ -0,0 +1,13 @@
+package test;
+import java.util.*;
+public class aly
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter any word: ");
+        String str = in.nextLine();
+        System.out.println(str);
+        in.close();
+    }
+}
